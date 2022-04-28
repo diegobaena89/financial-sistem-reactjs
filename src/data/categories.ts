cf5fdfe5524ff7b03cd819376b4eea8fdc6ed15c
diegobaena89 <diegobf.cello@gmail.com>
2022-04-28 16:35:25 -0300

    add types and data with ts

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../types/Categories";
+
+export const categories: Category = {
+  food: { title: "Alimetação", color: "#ef233c", expense: true },
+  rent: { title: "Aluguel", color: "#d90429", expense: true },
+  salary: { title: "Salário", color: "1a759f", expense: false },
+};
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Mc Donalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Burguer King",
+    value: 28,
+  },
+  {
+    date: new Date(2021, 9, 16),
+    category: "rent",
+    title: "Aluguel apt",
+    value: 2300,
+  },
+  {
+    date: new Date(2021, 10, 18),
+    category: "salary",
+    title: "Salário ACME",
+    value: 4500,
+  },
+];
